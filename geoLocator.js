20683b3e180b3eeba50aee3ffc922dab5a01dcbe
BorislavBosnic <bosnicbl@gmail.com>
2018-03-17 14:10:28 +0100

    Update geoLocator.js

--- geoLocator.js
+++ geoLocator.js
@@ -6,7 +6,7 @@ function httpGet(theUrl)
   
   
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.open( "GET", theUrl); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
